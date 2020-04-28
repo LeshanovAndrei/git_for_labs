11d4a7a365523963bd1f4e13cbc33c48ce92c0f1
kryvoshapka.andrew <kryvoshapka.andrew@gmail.com>
2020-04-28 23:27:34 +0300

    star fixed

--- LAB3/LAB3/star.cpp
+++ LAB3/LAB3/star.cpp
@@ -14,9 +14,9 @@ void Star::Draw(int r, int g, int b)
 	poly[1].y = centerY + (radius * 0.81);
 	poly[2].x = centerX + (radius + 0.951);
 	poly[2].y = centerY - (radius * 0.31);
-	poly[3].x = centerX + (radius + 0.951);
+	poly[3].x = centerX - (radius + 0.951);
 	poly[3].y = centerY - (radius * 0.31);
-	poly[4].x = centerX - (radius * 0.59);
+	poly[4].x = centerX + (radius * 0.59);
 	poly[4].y = centerY + (radius * 0.81);
 	poly[5].x = centerX;
 	poly[5].y = centerY - radius;
