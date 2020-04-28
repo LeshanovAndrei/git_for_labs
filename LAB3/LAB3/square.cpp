6f70f7b077aa955a7cb9ff9047b89fcfc753abad
kryvoshapka.andrew <kryvoshapka.andrew@gmail.com>
2020-04-28 23:38:13 +0300

    square fixed

--- LAB3/LAB3/square.cpp
+++ LAB3/LAB3/square.cpp
@@ -7,7 +7,18 @@ void Square::Draw(int r, int g, int b)
 {
 	HPEN hPen = CreatePen(PS_SOLID, 3, RGB(r, g, b)); //Объявляется и создается кисть
 	SelectObject(hdc, hPen); //Объект делается текущим
-	Rectangle(hdc, (centerX - (radius / (1.414))), (centerY - (radius / (1.414))), (centerX + (radius / (1.414))), (centerY + (radius / (1.414))));
+	POINT poly[5];
+	poly[0].x = centerX - (radius / 1.414);
+	poly[0].y = centerY - (radius / 1.414);
+	poly[1].x = centerX + (radius / 1.414);
+	poly[1].y = centerY - (radius / 1.414);
+	poly[2].x = centerX + (radius / 1.414);
+	poly[2].y = centerY + (radius / 1.414);
+	poly[3].x = centerX - (radius / 1.414);
+	poly[3].y = centerY + (radius / 1.414);
+	poly[4].x = centerX - (radius / 1.414);
+	poly[4].y = centerY - (radius / 1.414);
+	Polyline(hdc, poly, 5);
 	if (track)
 	{
 
